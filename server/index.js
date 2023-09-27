1f2e75cda2be0dfde81e2f04a720e67d41da60f6
SoulLess999 <amoghpnivas@gmail.com>
2023-09-27 19:19:17 +0530

    added client url in .env

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["https://formaker-6ib3.vercel.app", "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
   })
 );
 
