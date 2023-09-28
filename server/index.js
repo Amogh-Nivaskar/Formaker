e8330ef80f6bf55045e4a88c8b25fb3c1437e95b
SoulLess999 <amoghpnivas@gmail.com>
2023-09-28 18:37:21 +0530

    removing cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use();
 // {
 //   credentials: true,
 //   origin: process.env.CLIENT_URL,
