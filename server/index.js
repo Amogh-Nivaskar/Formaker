1d4d77799f69f5943cde7b53193baa1f6b743286
SoulLess999 <amoghpnivas@gmail.com>
2023-09-28 18:26:40 +0530

    adding access control headers to res

--- server/index.js
+++ server/index.js
@@ -19,13 +19,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
-
+app.use(cors());
+// {
+//   credentials: true,
+//   origin: process.env.CLIENT_URL,
+// }
 // app.get("/", (req, res) => {
 //   return res.status(200).json({ message: "Heres your data" });
 // });
@@ -33,6 +31,30 @@ app.use(
 // app.get("/trial", (req, res) => {
 //   return res.status(200).json({ message: "Heres your trial data" });
 // });
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use("/", userRoutes);
 app.use("/questionForm", verifyToken, questionFormRoutes);
