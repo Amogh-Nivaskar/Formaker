a1363a25f367fa34658bc2b85ada99fbe648709a
SoulLess999 <amoghpnivas@gmail.com>
2023-11-23 19:22:31 +0530

    removing-unnecessary-imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,4 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  RouterProvider,
-  Routes,
-  createBrowserRouter,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Root from "./ui/Root";
 import NewForm from "./Pages/NewForm";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,14 +6,13 @@ import "./index.css";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import { AuthProvider, useAuth } from "./contexts/UserAuth";
+import { useAuth } from "./contexts/UserAuth";
 import Protected from "./Components/Protected";
 import { useEffect } from "react";
 import axios from "axios";
 import AnsForm from "./Pages/AnsForm";
 import Submitted from "./Pages/Submitted";
 import ViewFormDetails from "./Pages/ViewFormDetails";
-import ViewSubmissions from "./Pages/PerPersonSubmissions";
 import PerPersonSubmissions from "./Pages/PerPersonSubmissions";
 import PerQuestionStats from "./Pages/PerQuestionStats";
 import SendByEmail from "./Pages/SendByEmail";
